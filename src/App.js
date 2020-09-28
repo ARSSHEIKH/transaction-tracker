2b8a7b2e889a7a93600c00396936514a8f927edf
Arsalan Sheikh <41226869+ARSSHEIKH@users.noreply.github.com>
2020-09-28 19:52:19 +0500

    used objects for transaction history list

---
+++ src/App.js
@@ -0,0 +1,17 @@
+import React from 'react';
+import Child from './child'
+// import {TransactionContext} from './transContext'
+
+import './App.css';
+
+function App() {
+  return (
+    <
+      <div className="App">
+        <Child/>
+      </div>
+   
+  );
+}
+
+export default App;
---
+++ src/child.js
@@ -0,0 +1,54 @@
+import React from 'react';
+import {TransactionContext} from './transContext'
+
+function Child() {
+    let transactions = [
+        { amount: 500, desc: "Cash" },
+        { amount: -40, desc: "Book" },
+        { amount: -200, desc: "Camera" }
+    ]
+    return (
+        <div className="main-container">
+            <h1 className="main-header-text">Expence Tracker</h1>
+            <h3>Your Balance <br />$260.00</h3>
+
+            <div className="expence-container">
+                <h3>INCOME <br />$500.00</h3>
+                <h3>EXPENCE <br />$240.00</h3>
+            </div>
+
+            <div className="history-container">
+                <h3>History</h3>
+                <hr /><br />
+                <ul className="history-list">
+                    {transactions.map((transObj, ind) => {
+                        return (
+                            <li>
+                                <span>{transObj.desc} </span>
+                                <span>{transObj.amount}</span>
+                            </li>
+                        );
+                    })}
+                </ul>
+            </div>
+
+            <div className="transaction-form">
+                <h3>Add New Transaction</h3>
+                <hr />
+                <label>
+                    Enter Description:
+                <input type="text" placeholder="Enter Description ..." required />
+                </label>
+                <br />
+                <label>
+                    Enter Amount:
+                <input type="number" placeholder="Enter Amount like (e.g: 1234...)" required />
+                </label>
+                <br />
+                <input type="submit" value="Add Transaction" />
+            </div>
+
+        </div>
+    );
+}
+export default Child;
