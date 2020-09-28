a0c09931697a9f97814bf87c9db1efb23adb20fe
Arsalan <arsheikh665@gmail.com>
2020-09-28 20:40:02 +0500

    Updated files
    now used Context-api for the list of transation history.

--- src/child.js
+++ src/child.js
@@ -1,12 +1,8 @@
-import React from 'react';
+import React, { useContext } from 'react';
 import {TransactionContext} from './transContext'
 
 function Child() {
-    let transactions = [
-        { amount: 500, desc: "Cash" },
-        { amount: -40, desc: "Book" },
-        { amount: -200, desc: "Camera" }
-    ]
+    let transactions = useContext(TransactionContext);
     return (
         <div className="main-container">
             <h1 className="main-header-text">Expence Tracker</h1>
--- src/transContext.js
+++ src/transContext.js
@@ -1,9 +1,24 @@
-import {createContext} from 'react'
+import {createContext, useReducer} from 'react'
+import TransactionReducer from './transReducer'
 
 const intitialTransactions = [
-    { amount: 500, desc: "Cash" },
+    { amount: 100, desc: "Cash" },
     { amount: -40, desc: "Book" },
     { amount: -200, desc: "Camera" }
 ]
 
 export const TransactionContext = createContext(intitialTransactions);
+
+export const TransactionProvider = () => {
+    let [state, dispatch] = useReducer(TransactionReducer, intitialTransactions);
+
+    function addTransaction(transObj){
+        dispatch({
+            type : "ADD_TRANSACTION",
+            payload: {
+                amount: transObj.createContext,
+                desc: transObj.desc
+            }
+        })
+    }
+}
